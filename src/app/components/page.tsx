2c6c442cd7210adda918513072f33214b7c871b2
Felippe Vilas Boas <fsvboas.dev@gmail.com>
2025-09-27 15:55:27 -0300

    refactor: add auth guard into pages

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,10 +1,13 @@
 import ComponentsDemonstration from "@/src/components/components-demonstration";
+import AuthGuard from "@/src/components/core/auth-guard";
 import Column from "@/src/components/core/column";
 
 export default function ComponentsPage() {
   return (
-    <Column className="w-full justify-center items-center space-y-4">
-      <ComponentsDemonstration />
-    </Column>
+    <AuthGuard requireAuth={false} redirectTo="/">
+      <Column className="w-full justify-center items-center space-y-4">
+        <ComponentsDemonstration />
+      </Column>
+    </AuthGuard>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import AuthGuard from "../components/core/auth-guard";
 import Column from "../components/core/column";
 
 export default function Home() {
   return (
-    <Column className="w-full justify-center items-center">
-      <h1 className="text-2xl font-bold">Precific</h1>
-    </Column>
+    <AuthGuard requireAuth>
+      <Column className="w-full justify-center items-center">
+        <h1 className="text-2xl font-bold">Precific Private Page</h1>
+      </Column>
+    </AuthGuard>
   );
 }
