272f1fd19431db6fdc7969c1d32f207272e4e694
fsvboas <fsvboas.dev@gmail.com>
2025-10-02 00:19:13 -0300

    refactor: update root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AppSidebar } from "../components/app-sidebar";
-import { SidebarTrigger } from "../components/core";
 import { Toaster } from "../components/core/sonner";
 import Providers from "../providers";
 import "./globals.css";
@@ -29,10 +28,7 @@ export default function RootLayout({
       >
         <Providers>
           <AppSidebar />
-          <main className="min-h-screen mr-7 w-full flex">
-            <SidebarTrigger />
-            <div className="flex w-full my-7">{children}</div>
-          </main>
+          <main className="min-h-screen mr-7 my-7 w-full flex">{children}</main>
           <Toaster />
         </Providers>
       </body>
