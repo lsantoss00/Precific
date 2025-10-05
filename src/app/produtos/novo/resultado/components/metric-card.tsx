74194cbf47a66a6504f2822023c7f20f146a9748
Felippe Vilas Boas <fsvboas.dev@gmail.com>
2025-10-05 15:58:49 -0300

    feat: create metric-card component

---
+++ src/app/produtos/novo/resultado/components/metric-card.tsx
@@ -0,0 +1,31 @@
+import Column from "@/src/components/core/column";
+import { currencyFormatter } from "@/src/helpers/currency-formatter";
+
+type VariantType = "primary" | "secondary" | "neutral" | "success" | "error";
+
+interface MetricCardProps {
+  title: string;
+  value: string | number;
+  variant?: VariantType;
+}
+
+const MetricCard = ({ title, value, variant = "neutral" }: MetricCardProps) => {
+  return (
+    <Column
+      className={`space-y-2 rounded-md p-4 border-2 ${variantStyles[variant]}`}
+    >
+      <p className="text-2xl font-bold">{currencyFormatter(value)}</p>
+      <span className="text-sm opacity-80">{title}</span>
+    </Column>
+  );
+};
+
+export default MetricCard;
+
+const variantStyles: Record<VariantType, string> = {
+  primary: "bg-[#66289B] text-white",
+  secondary: "bg-[#E9BA67] text-white",
+  neutral: "bg-gray-200 text-black",
+  success: "bg-green-600 text-white",
+  error: "bg-red-600 text-white",
+};
