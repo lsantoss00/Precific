16d99561acdc8cf119217785b53350f6137a295d
Felippe Vilas Boas <fsvboas.dev@gmail.com>
2025-10-05 12:28:31 -0300

    refactor: add inputs to precification-form (just layout)

--- src/app/produtos/novo/components/precification-form.tsx
+++ src/app/produtos/novo/components/precification-form.tsx
@@ -1,12 +1,36 @@
-import { Card } from "@/src/components/core";
+import { Card, Input, Label } from "@/src/components/core";
 import Column from "@/src/components/core/column";
 
 const PrecificationForm = () => {
   return (
-    <Card className="h-full w-full p-6 rounded-md">
-      <Column>
-        <h3>Precificação</h3>
-      </Column>
+    <Card className="h-full w-full p-6 rounded-md flex space-y-6">
+      <h3>Precificação</h3>
+      <form className="grid grid-cols-2 gap-4">
+        <Column className="space-y-2">
+          <Label htmlFor="fixedCosts">Custos Fixos (%)</Label>
+          <Input id="fixedCosts" type="number" placeholder="0,00%" />
+        </Column>
+        <Column className="space-y-2">
+          <Label htmlFor="icms">ICMS Venda (%)</Label>
+          <Input id="icms" type="number" placeholder="0,00%" />
+        </Column>
+        <Column className="space-y-2">
+          <Label htmlFor="pisCofins">PIS/COFINS Venda (%)</Label>
+          <Input id="pisCofins" type="number" placeholder="0,00%" />
+        </Column>
+        <Column className="space-y-2">
+          <Label htmlFor="freight">Frete (%)</Label>
+          <Input id="freight" type="number" placeholder="0,00%" />
+        </Column>
+        <Column className="space-y-2">
+          <Label htmlFor="otherCosts">Outros Custos (%)</Label>
+          <Input id="otherCosts" type="number" placeholder="0,00%" />
+        </Column>
+        <Column className="space-y-2">
+          <Label htmlFor="profitMargin">Margem de Lucro (%)</Label>
+          <Input id="profitMargin" type="number" placeholder="0,00%" />
+        </Column>
+      </form>
     </Card>
   );
 };
