28abf8ca30f1789c2febec5f49ad50e44da8e724
fsvboas <fsvboas.dev@gmail.com>
2025-10-07 11:05:05 -0300

    refactor: update tanstack-query-provider adding a separate file to queryClient

--- src/providers/tanstack-query-provider.tsx
+++ src/providers/tanstack-query-provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "../libs/tanstack-query/query-client";
 
 interface TanstackQueryProviderProps {
   children: React.ReactNode;
@@ -9,8 +10,6 @@ interface TanstackQueryProviderProps {
 export const TanstackQueryProvider = ({
   children,
 }: TanstackQueryProviderProps) => {
-  const queryClient = new QueryClient();
-
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
