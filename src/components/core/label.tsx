9de943da9bc6024abb5bea15cfdf8e3505806656
fsvboas <fsvboas.dev@gmail.com>
2025-10-07 22:12:57 -0300

    refactor: adjust label component

--- src/components/core/label.tsx
+++ src/components/core/label.tsx
@@ -4,7 +4,6 @@ import * as LabelPrimitive from "@radix-ui/react-label";
 import * as React from "react";
 
 import { cn } from "@/src/libs/shadcn-ui/utils";
-import Row from "./row";
 import Show from "./show";
 
 interface LabelProps extends React.ComponentProps<typeof LabelPrimitive.Root> {
@@ -13,19 +12,19 @@ interface LabelProps extends React.ComponentProps<typeof LabelPrimitive.Root> {
 
 function Label({ className, required = false, ...props }: LabelProps) {
   return (
-    <Row className="gap-1">
-      <LabelPrimitive.Root
-        data-slot="label"
-        className={cn(
-          "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
-          className
-        )}
-        {...props}
-      />
+    <LabelPrimitive.Root
+      data-slot="label"
+      className={cn(
+        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+        className
+      )}
+      {...props}
+    >
+      {props.children}
       <Show when={required}>
         <span className="text-red-500">*</span>
       </Show>
-    </Row>
+    </LabelPrimitive.Root>
   );
 }
 
