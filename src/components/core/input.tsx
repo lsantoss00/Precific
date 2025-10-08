0113e6419b5e300497ba5a0242f6043df6009d50
fsvboas <fsvboas.dev@gmail.com>
2025-10-08 11:26:51 -0300

    refactor: update input component to show error state

--- src/components/core/input.tsx
+++ src/components/core/input.tsx
@@ -1,20 +1,31 @@
 import * as React from "react";
 
 import { cn } from "@/src/libs/shadcn-ui/utils";
+import Show from "./show";
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: string;
+}
+
+function Input({ className, type, error, ...props }: InputProps) {
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground border h-9 w-full min-w-0 rounded-md bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-[#66289B] focus-visible:ring-[#66289B]/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 aria-invalid:border-destructive h-12 bg-white",
-        className
-      )}
-      {...props}
-    />
+    <>
+      <input
+        type={type}
+        data-slot="input"
+        className={cn(
+          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground border h-9 w-full min-w-0 rounded-md bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "focus-visible:border-[#66289B] focus-visible:ring-[#66289B]/50 focus-visible:ring-[3px]",
+          "aria-invalid:ring-destructive/20 aria-invalid:border-destructive h-12 bg-white",
+          error && "border-red-500",
+          className
+        )}
+        {...props}
+      />
+      <Show when={error}>
+        <span className="text-xs text-red-500 -mt-1">{error}</span>
+      </Show>
+    </>
   );
 }
 
