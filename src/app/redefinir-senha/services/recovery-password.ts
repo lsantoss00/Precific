9f6b7e6064c7bad00aa0ac2c87f07dc76132d095
fsvboas <fsvboas.dev@gmail.com>
2025-10-09 00:42:14 -0300

    refactor: remove unnecessary redirectTo from recovery-password service

--- src/app/redefinir-senha/services/recovery-password.ts
+++ src/app/redefinir-senha/services/recovery-password.ts
@@ -6,10 +6,7 @@ interface RecoveryPasswordProps {
 
 export async function recoveryPassword({ email }: RecoveryPasswordProps) {
   const { data, error } = await supabaseClient.auth.resetPasswordForEmail(
-    email,
-    {
-      redirectTo: `${window.location.origin}/criar-nova-senha`,
-    }
+    email
   );
 
   if (error) {
