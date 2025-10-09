66e9d91244ae30b494d4db9a746f2a2f99822764
fsvboas <fsvboas.dev@gmail.com>
2025-10-09 11:56:51 -0300

    refactor: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
 "use client";
 
-import { redirect } from "next/navigation";
+import { useAuth } from "@/src/hooks/use-auth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return redirect("/produtos");
+  const router = useRouter();
+  const { isAuthenticated, loading } = useAuth();
+
+  useEffect(() => {
+    if (loading) return;
+
+    if (isAuthenticated) {
+      router.push("/produtos");
+    } else {
+      router.push("/entrar");
+    }
+  }, [isAuthenticated, loading, router]);
+
+  if (loading) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-[#66289B]" />
+      </div>
+    );
+  }
+
+  return null;
 }
