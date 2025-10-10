dcdaf1ca05b9cbc5270cf0024cffc4383cb0660f
fsvboas <fsvboas.dev@gmail.com>
2025-10-10 20:26:22 -0300

    refactor: add default values to product-form useForm

--- src/app/produtos/contexts/product-form-context.tsx
+++ src/app/produtos/contexts/product-form-context.tsx
@@ -33,6 +33,24 @@ export const ProductFormProvider = ({
 }: ProductFormProviderProps) => {
   const form = useForm<ProductFormDataType>({
     mode: "onChange",
+    defaultValues: {
+      name: "",
+      sku: "",
+      ncm: "",
+      observations: "",
+      unit_price: 0,
+      icms: 0,
+      pis_cofins: 0,
+      icms_st: 0,
+      ipi: 0,
+      others: 0,
+      fixed_costs: 0,
+      sales_icms: 0,
+      sales_pis_cofins: 0,
+      shipping: 0,
+      other_costs: 0,
+      profit: 0,
+    },
   });
 
   return (
