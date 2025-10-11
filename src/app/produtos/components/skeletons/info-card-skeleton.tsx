705517ef4ebfb8bb109bab0cc905a96f4828574b
Felippe Vilas Boas <fsvboas.dev@gmail.com>
2025-10-11 15:17:49 -0300

    feat: create info-card-skeleton

---
+++ src/app/produtos/components/skeletons/info-card-skeleton.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "@/src/components/core";
+
+const InfoCardSkeleton = () => {
+  return <Skeleton className="w-full h-28 rounded-md" />;
+};
+
+export default InfoCardSkeleton;
