ec079b1c6af4f3f06f7fb9be8a495256649a4df3
Felippe Vilas Boas <fsvboas.dev@gmail.com>
2025-10-12 16:36:03 -0300

    feat: initial layout to form-field tooltip

---
+++ src/components/form-field-tooltip.tsx
@@ -0,0 +1,39 @@
+import { CircleQuestionMark } from "lucide-react";
+import { useState } from "react";
+import { Button } from "./core";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./core/tooltip";
+
+interface FormFieldsTooltipProps {
+  message: string;
+}
+
+const FormFieldTooltip = ({ message }: FormFieldsTooltipProps) => {
+  const [openTooltip, setOpenTooltip] = useState<boolean>(false);
+
+  return (
+    <Tooltip open={openTooltip} onOpenChange={setOpenTooltip}>
+      <TooltipTrigger asChild>
+        <Button
+          type="button"
+          className="bg-transparent shadow-none text-primary hover:bg-transparent !p-0 h-fit"
+          onClick={() => setOpenTooltip(!openTooltip)}
+          onMouseEnter={() => setOpenTooltip(true)}
+          onMouseLeave={() => setOpenTooltip(false)}
+          onTouchStart={() => setOpenTooltip(!openTooltip)}
+          onKeyDown={(event) => {
+            event.preventDefault();
+            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+            event.key === "Enter" && setOpenTooltip(!openTooltip);
+          }}
+        >
+          <CircleQuestionMark size={24} />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{message}</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+};
+
+export default FormFieldTooltip;
