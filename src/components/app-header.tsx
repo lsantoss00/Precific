7d64852a054573bbd27798596205524ed8f9e63e
fsvboas <fsvboas.dev@gmail.com>
2025-10-18 12:32:58 -0300

    refactor: try to fix padding in notebooks

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -13,7 +13,7 @@ export function AppHeader() {
 
   return (
     <header className="bg-[#fafafa] min-h-20 max-h-20 text-white p-6 sticky top-0 z-10 border-b flex shrink-0">
-      <Row className="w-full h-full !max-w-7xl xl:!max-w-6xl 2xl:!max-w-[1500px] justify-between xl:!justify-end items-center mx-auto">
+      <Row className="w-full h-full max-w-7xl xl:max-w-5xl 2xl:max-w-[1500px] justify-between xl:justify-end items-center mx-auto">
         <SidebarTrigger className="!p-0 xl:!hidden">
           <Image
             src="/precific-short-logo.png"
