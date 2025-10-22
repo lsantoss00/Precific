b31f6e92c6bad78630c4ca4dc80d2fd0a934d2dd
lsantoss00 <lucassantossilva100@gmail.com>
2025-10-22 17:28:53 -0300

    refactor: comments calc changes

--- src/app/(private)/produtos/utils/price-today-calc.ts
+++ src/app/(private)/produtos/utils/price-today-calc.ts
@@ -17,16 +17,20 @@ export function priceTodayCalc({
   salesIcms,
   salesPisCofins,
 }: PriceTodayCalcProps) {
-  const percentSum = (fixedCosts + shipping + othersCost + profit) / 100;
+  const percentSum = VFA + (fixedCosts + shipping + othersCost) / 100;
 
   const value = acquisitionCost + acquisitionCost * percentSum;
+
+  //plus - > const 1base = value + value * (profit / 100);
 
   const salesIcmsPercent = salesIcms / 100;
   const salesPisCofinsPercent = salesPisCofins / 100;
 
-  const denominator = 1 - salesIcmsPercent - salesPisCofinsPercent; // GABRIELA MANDOU TIRAR: * (1 - salesIcmsPercent)
+  const denominator = 1 - salesIcmsPercent - salesPisCofinsPercent; // GABRIELLA MANDOU TIRAR: * (1 - salesIcmsPercent)
 
   const priceToday = value / denominator;
+
+  //change - > const priceToday = 1base / denominator;
 
   const result = priceToday.toFixed(2);
 
@@ -35,3 +39,15 @@ export function priceTodayCalc({
     value: Number(value),
   };
 }
+
+// BC_IRPJ_CSLL = priceToday - fixedCosts - shipping - othersCost - unitPrice - ICMS_venda_value - pisCofins_venda_value
+
+// percentIRPJ_CSLL = 34 / 100
+// IRPJ_CSLL = BC_IRPJ_CSLL * percentIRPJ_CSLL
+
+// recICMS = ICMS_venda_value - ICMS_compra_value
+// recPIS_COFINS = pisCofins_venda_value - pisCofins_compra_value
+
+// card results \/
+// LUCRO_LIQUIDO = priceToday - fixedCosts - shipping - othersCost - unitPrice - IRPJ_CSLL - recICMS - recPIS_COFINS
+// margemLiq = LUCRO_LIQUIDO / priceToday
