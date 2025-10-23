5ac974d88629afd02985c5f4dbb1325b558700cc
fsvboas <fsvboas.dev@gmail.com>
2025-10-23 00:04:00 -0300

    refactor: update middleware to avoid redirect

--- src/libs/supabase/middleware.ts
+++ src/libs/supabase/middleware.ts
@@ -10,26 +10,21 @@ const PUBLIC_ROUTES = [
 
 export async function updateSession(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
+  const hasErrorParam = request.nextUrl.searchParams.has("error");
 
-  let supabaseResponse = NextResponse.next({
-    request,
-  });
+  let supabaseResponse = NextResponse.next({ request });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
+        getAll: () => request.cookies.getAll(),
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
+          cookiesToSet.forEach(({ name, value }) =>
             request.cookies.set(name, value)
           );
-          supabaseResponse = NextResponse.next({
-            request,
-          });
+          supabaseResponse = NextResponse.next({ request });
           cookiesToSet.forEach(({ name, value, options }) =>
             supabaseResponse.cookies.set(name, value, options)
           );
@@ -49,18 +44,28 @@ export async function updateSession(request: NextRequest) {
   if (!user && !isPublicRoute) {
     const url = request.nextUrl.clone();
     url.pathname = "/entrar";
+    url.search = "";
     return NextResponse.redirect(url);
   }
 
-  if (
-    user &&
-    (pathname === "/entrar" ||
-      pathname === "/redefinir-senha" ||
-      pathname === "/")
-  ) {
-    const url = request.nextUrl.clone();
-    url.pathname = "/produtos";
-    return NextResponse.redirect(url);
+  if (user) {
+    if (pathname === "/entrar" || pathname === "/") {
+      const url = request.nextUrl.clone();
+      url.pathname = "/produtos";
+      url.search = "";
+      return NextResponse.redirect(url);
+    }
+
+    if (pathname.startsWith("/redefinir-senha")) {
+      if (!hasErrorParam) {
+        const url = request.nextUrl.clone();
+        url.pathname = "/produtos";
+        url.search = "";
+        return NextResponse.redirect(url);
+      }
+      return supabaseResponse;
+    }
   }
+
   return supabaseResponse;
 }
