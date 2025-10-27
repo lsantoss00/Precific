79018092a96c16914f98d229e37c961a3b70de59
fsvboas <fsvboas.dev@gmail.com>
2025-10-26 21:13:45 -0300

    fix: user logging when refresh recovery-password page

--- src/app/(public)/redefinir-senha/components/recovery-password-card.tsx
+++ src/app/(public)/redefinir-senha/components/recovery-password-card.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import { logout } from "@/src/app/(public)/entrar/services/logout";
 import { Card } from "@/src/components/core";
 import Image from "next/image";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import RecoveryPasswordForm from "./recovery-password-form";
 
 const RecoveryPasswordCard = () => {
+  useEffect(() => {
+    (async () => {
+      await logout();
+    })();
+  }, []);
+
   return (
     <Card className="rounded-2xl p-6 border shadow-sm max-w-125 w-full max-h-125 flex justify-center bg-white">
       <Image
