b8c4401adf5c9a5ed6e379366a0ff3aa9b7f0a22
fsvboas <fsvboas.dev@gmail.com>
2025-10-30 02:36:08 -0300

    feat: create masked-input component

---
+++ src/components/core/masked-input.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { cn } from "@/src/libs/shadcn-ui/utils";
+import { forwardRef } from "react";
+import { IMaskInput } from "react-imask";
+
+interface MaskedInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  mask: string;
+  value: string;
+  onAccept: (value: string) => void;
+  unmask?: boolean;
+  error?: string;
+}
+
+const MaskedInput = forwardRef<HTMLInputElement, MaskedInputProps>(
+  (
+    { mask, value, onAccept, className, unmask = true, error, ...props },
+    ref
+  ) => {
+    return (
+      <IMaskInput
+        mask={mask}
+        value={value}
+        unmask={unmask}
+        onAccept={onAccept}
+        inputRef={ref}
+        className={cn(
+          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground border w-full min-w-0 rounded-md bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "focus-visible:border-[#66289B] focus-visible:ring-[#66289B]/50 focus-visible:ring-[3px]",
+          "aria-invalid:ring-destructive/20 aria-invalid:border-destructive h-12 bg-white",
+          error && "border-red-500",
+          className
+        )}
+        {...props}
+      />
+    );
+  }
+);
+
+MaskedInput.displayName = "MaskedInput";
+
+export { MaskedInput };
