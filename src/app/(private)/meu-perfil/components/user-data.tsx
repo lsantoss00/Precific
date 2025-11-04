3cb7046decf284c9a5ff8e2bdc8c9fcc825894a3
fsvboas <fsvboas.dev@gmail.com>
2025-11-04 01:39:28 -0300

    feat: create coming-soon-badge and add to profile picture input

--- src/app/(private)/meu-perfil/components/user-data.tsx
+++ src/app/(private)/meu-perfil/components/user-data.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import UserImageUpload from "@/src/app/(private)/meu-perfil/components/user-image-upload";
+import ComingSoonBadge from "@/src/components/coming-soon-badge";
 import { Card, Input, Label } from "@/src/components/core";
 import Column from "@/src/components/core/column";
+import Row from "@/src/components/core/row";
 import { useAuth } from "@/src/providers/auth-provider";
 
 const UserData = () => {
@@ -13,7 +15,10 @@ const UserData = () => {
       <h3>Dados do Usuário</h3>
       <div className="space-y-4">
         <Column className="space-y-2">
-          <Label htmlFor="profilePicture">Foto de Perfil</Label>
+          <Row className="gap-2 items-center w-full">
+            <Label htmlFor="profilePicture">Foto de Perfil</Label>
+            <ComingSoonBadge />
+          </Row>
           <UserImageUpload
             disabled
             onChange={(file, preview) => {
---
+++ src/components/coming-soon-badge.tsx
@@ -0,0 +1,11 @@
+import Row from "@/src/components/core/row";
+
+const ComingSoonBadge = () => {
+  return (
+    <Row className="rounded-full bg-neutral-900 p-2 h-2 w-fit items-center">
+      <span className="text-neutral-50 text-xs">Em breve</span>
+    </Row>
+  );
+};
+
+export default ComingSoonBadge;
