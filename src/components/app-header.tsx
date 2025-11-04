aa6d364ae39ae7fbb1aa48e3ec96544a59458c48
fsvboas <fsvboas.dev@gmail.com>
2025-11-03 23:53:35 -0300

    refactor: minor adjust to app-header component

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -7,7 +7,7 @@ import Menu from "./menu";
 
 export function AppHeader() {
   return (
-    <header className="bg-[#fafafa] min-h-20 max-h-20 text-white p-6 sticky top-0 z-50 border-b flex shrink-0">
+    <header className="bg-[#fafafa] text-white p-6 sticky top-0 z-50 border-b flex shrink-0">
       <Row className="w-full h-full max-w-7xl xl:max-w-5xl 2xl:max-w-[1500px] justify-between xl:justify-end items-center mx-auto">
         <SidebarTrigger className="!p-0 xl:!hidden">
           <Image
