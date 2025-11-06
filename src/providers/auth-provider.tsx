2c882935140ccaada6493cc42fa0ec58de7fd86c
fsvboas <fsvboas.dev@gmail.com>
2025-11-06 16:21:03 -0300

    refactor: improve auth provider

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -1,56 +1,59 @@
 "use client";
 
 import { getUserProfile } from "@/src/app/(private)/perfil/services/get-user-profile";
+import { queryClient } from "@/src/libs/tanstack-query/query-client";
 import { User } from "@supabase/supabase-js";
 import { useQuery } from "@tanstack/react-query";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { createClient } from "../libs/supabase/client";
 
 interface AuthContextType {
   user: User | null;
-  hasCompany: boolean;
+  profile: any;
 }
 
 const AuthContext = createContext<AuthContextType>({
   user: null,
-  hasCompany: false,
+  profile: null,
 });
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState<User | null>(null);
+  const supabase = createClient();
 
-  useEffect(() => {
-    const supabase = createClient();
-
-    const getUser = async () => {
+  const { data: user } = useQuery({
+    queryFn: async () => {
       const {
         data: { user },
       } = await supabase.auth.getUser();
-      setUser(user);
-    };
-    getUser();
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setUser(session?.user ?? null);
-    });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, []);
+      return user;
+    },
+    queryKey: ["user"],
+    staleTime: Infinity,
+  });
 
   const { data: profile } = useQuery({
     queryFn: () => getUserProfile({ userId: user!.id }),
-    queryKey: ["userProfile", user?.id],
+    queryKey: ["profile", user?.id],
     enabled: !!user?.id,
   });
 
-  const hasCompany = !!profile?.company_id;
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(() => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ user, hasCompany }}>
+    <AuthContext.Provider
+      value={{
+        user: user ?? null,
+        profile,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
