9f716098482cc19bba0b44c946c7c7cd15ad24fd
fsvboas <fsvboas.dev@gmail.com>
2025-11-12 00:01:24 -0300

    refactor: minor adjustment to auth-form-card componnet

--- src/components/auth-form-card.tsx
+++ src/components/auth-form-card.tsx
@@ -8,14 +8,15 @@ interface AuthFormCardProps {
 const AuthFormCard = ({ children }: AuthFormCardProps) => {
   return (
     <Card className="rounded-2xl p-6 shadow-sm w-full max-w-125 max-h-125 flex justify-center bg-white">
-      <Image
-        src="/precific-logo-image.webp"
-        alt="precific-logo-image"
-        width={300}
-        height={100}
-        priority
-        className="flex self-center mb-10"
-      />
+      <div className="flex self-center mb-10">
+        <Image
+          src="/precific-logo-image.webp"
+          alt="precific-logo-image"
+          width={300}
+          height={100}
+          priority
+        />
+      </div>
       {children}
     </Card>
   );
