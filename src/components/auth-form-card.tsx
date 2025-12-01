58f94c2038167126c32df66e722612cf271b8b3d
fsvboas <fsvboas.dev@gmail.com>
2025-12-01 15:03:02 -0300

    refactor: update auth-form-card component

--- src/components/auth-form-card.tsx
+++ src/components/auth-form-card.tsx
@@ -8,15 +8,14 @@ interface AuthFormCardProps {
 const AuthFormCard = ({ children }: AuthFormCardProps) => {
   return (
     <Card className="rounded-2xl p-6 shadow-sm w-full max-w-125 max-h-125 flex justify-center bg-white">
-      <div className="flex self-center mb-10 w-[300px] h-[80px] relative">
-        <Image
-          src="/precific-logo-image.webp"
-          alt="precific-logo-image"
-          fill
-          className="object-contain"
-          priority
-        />
-      </div>
+      <Image
+        src="/precific-logo-image.webp"
+        alt="precific-logo-image"
+        width={300}
+        height={80}
+        className="object-contain mb-10 self-center"
+        priority
+      />
       {children}
     </Card>
   );
