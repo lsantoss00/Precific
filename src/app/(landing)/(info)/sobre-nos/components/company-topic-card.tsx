c466ea2badb45d3b28bc47373c98a845afeb54de
fsvboas <fsvboas.dev@gmail.com>
2025-12-12 20:17:54 -0300

    refactor: add a shadow to company-topic-card component

--- src/app/(landing)/(info)/sobre-nos/components/company-topic-card.tsx
+++ src/app/(landing)/(info)/sobre-nos/components/company-topic-card.tsx
@@ -11,7 +11,7 @@ const CompanyTopicCard = ({
   title,
   description,
 }: CompanyTopicCardProps) => (
-  <Column className="relative w-full xl:max-w-2xl items-center justify-center space-y-6 rounded-lg bg-black/40 px-4 py-6 lg:h-60 lg:flex-row lg:space-y-0 lg:px-0 lg:py-0">
+  <Column className="relative w-full xl:max-w-2xl items-center justify-center space-y-6 rounded-lg bg-black/40 shadow-2xl px-4 py-6 lg:h-60 lg:flex-row lg:space-y-0 lg:px-0 lg:py-0">
     <Column className="w-full lg:w-auto">
       <span className="-left-10 top-20 h-fit w-fit rounded-full bg-secondary p-3 text-white lg:absolute lg:p-6">
         {icon}
