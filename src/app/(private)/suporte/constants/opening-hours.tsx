5af424f0d59f1f6f9173a782b341a6b64639009c
fsvboas <fsvboas.dev@gmail.com>
2025-12-24 03:08:19 -0300

    refactor: file extension change

--- src/app/(private)/suporte/constants/opening-hours.tsx
+++
@@ -1,9 +0,0 @@
-export const openingHours = [
-  { day: "Segunda-feira", hours: "08h - 18h30", isOpen: true },
-  { day: "Terça-feira", hours: "08h - 18h30", isOpen: true },
-  { day: "Quarta-feira", hours: "08h - 18h30", isOpen: true },
-  { day: "Quinta-feira", hours: "08h - 18h30", isOpen: true },
-  { day: "Sexta-feira", hours: "09h - 15h", isOpen: true },
-  { day: "Sábado", hours: "Fechado", isOpen: false },
-  { day: "Domingo", hours: "Fechado", isOpen: false },
-];
