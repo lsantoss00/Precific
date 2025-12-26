8ee01d0f7e85562ddb96b612ee39cad920cb3a6d
fsvboas <fsvboas.dev@gmail.com>
2025-12-26 12:36:25 -0300

    refactor: simplify Container component in DashboardPage

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -40,10 +40,7 @@ export const metadata: Metadata = {
 
 export default function DashboardPage() {
   return (
-    <Container
-      variant="page"
-      className="max-w-7xl xl:max-w-5xl 2xl:max-w-[1500px] space-y-4 flex flex-col"
-    >
+    <Container variant="page">
       <Row className="items-center gap-4">
         <h1 className="text-3xl text-black font-bold">Dashboard</h1>
         <ComingSoonBadge />
