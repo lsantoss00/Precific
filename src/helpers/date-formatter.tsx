e065786648108fd7163156d4fd14f255d45606d7
fsvboas <fsvboas.dev@gmail.com>
2025-12-30 18:06:04 -0300

    refactor: improve date-formatter helper

--- src/helpers/date-formatter.tsx
+++ src/helpers/date-formatter.tsx
@@ -2,18 +2,22 @@ import { format, isValid, parseISO } from "date-fns";
 
 interface DateFormatterProps {
   children: string | undefined;
+  showTime?: boolean;
 }
 
-const dateFormatter = (dateAsString: string) => {
+const dateFormatter = (dateAsString: string, showTime?: boolean) => {
   const date = parseISO(dateAsString);
-  return isValid(date) ? format(date, "dd/MM/yyyy") : undefined;
+  if (!isValid(date)) return undefined;
+  return showTime
+    ? format(date, "dd/MM/yyyy 'às' HH:mm:ss")
+    : format(date, "dd/MM/yyyy");
 };
 
-const DateFormatter = ({ children }: DateFormatterProps) => {
-  if (!children) return;
+const DateFormatter = ({ children, showTime = false }: DateFormatterProps) => {
+  if (!children) return null;
   const date = parseISO(children);
   if (!isValid(date)) return null;
-  return <>{dateFormatter(children)}</>;
+  return <>{dateFormatter(children, showTime)}</>;
 };
 
 export default DateFormatter;
