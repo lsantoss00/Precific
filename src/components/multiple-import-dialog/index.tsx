bd61bb4a985355699ffba5bbc375782f1eb8f8fa
fsvboas <fsvboas.dev@gmail.com>
2026-01-02 19:31:11 -0300

    refactor: minor layout change do multiple-import-dialog component

--- src/components/multiple-import-dialog/index.tsx
+++ src/components/multiple-import-dialog/index.tsx
@@ -40,7 +40,7 @@ const MultipleImportDialog = ({ trigger }: MultipleImportDialogProps) => {
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen} modal>
       <DialogTrigger asChild>{trigger}</DialogTrigger>
-      <DialogContent className="flex flex-col md:min-w-2xl md:py-12 md:px-12 h-[96vh]">
+      <DialogContent className="flex flex-col md:min-w-2xl md:py-12 md:px-12">
         {isImporting ? (
           <MultipleImportLoadingState progress={importProgress} />
         ) : (
