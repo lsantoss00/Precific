56f933cf688e57aa8ca17871cba6d6bec9547462
fsvboas <fsvboas.dev@gmail.com>
2026-01-13 20:13:31 -0300

    refactor: improve input component

--- src/components/core/input.tsx
+++ src/components/core/input.tsx
@@ -1,25 +1,42 @@
+"use client";
+
 import { Button } from "@/src/components/core/button";
 import Show from "@/src/components/core/show";
+import { useMediaQuery } from "@/src/hooks/use-media-query";
 import { cn } from "@/src/libs/shadcn-ui/utils";
 import { Eye, EyeOff } from "lucide-react";
-import * as React from "react";
+import { forwardRef, useRef, useState } from "react";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: string;
 }
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, error, ...props }, ref) => {
-    const [showPassword, setShowPassword] = React.useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const isMobile = useMediaQuery("(max-width: 768px)");
+
     const isPassword = type === "password";
 
     const inputType = isPassword && showPassword ? "text" : type;
+
+    const handleFocus = () => {
+      const input = inputRef.current;
+      if (!input) return;
+
+      if (isMobile)
+        return setTimeout(() => {
+          input.scrollIntoView({ behavior: "smooth", block: "center" });
+        }, 200);
+    };
 
     return (
       <div className="relative w-full">
         <input
           ref={ref}
           type={inputType}
+          onFocus={handleFocus}
           data-slot="input"
           className={cn(
             "file:text-foreground placeholder:text-muted-foreground border w-full min-w-0 rounded-md bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
