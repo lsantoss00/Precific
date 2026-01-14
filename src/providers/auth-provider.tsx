5ab610c4512da519e96a4769cdca65bcbf996d0d
fsvboas <fsvboas.dev@gmail.com>
2026-01-14 17:44:02 -0300

    feat: improve auth-provider

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -3,16 +3,19 @@
 import { getCompanyById } from "@/src/app/(private)/perfil/services/get-company-by-id";
 import { getCompanySubscriptionStatus } from "@/src/app/(private)/perfil/services/get-company-subscription-status";
 import { getUserProfile } from "@/src/app/(private)/perfil/services/get-user-profile";
+import { CompanyType } from "@/src/app/(private)/perfil/types/company-type";
+import { ProfileType } from "@/src/app/(private)/perfil/types/profile-type";
 import { queryClient } from "@/src/libs/tanstack-query/query-client";
+import { User } from "@supabase/supabase-js";
 import { useQuery } from "@tanstack/react-query";
 import { createContext, useContext, useEffect } from "react";
 import { createClient } from "../libs/supabase/client";
 
 interface AuthContextType {
-  profile: any;
-  company: any;
+  profile: ProfileType | null;
+  company: CompanyType | null;
   isPremium: boolean;
-  expiresAt?: string | null;
+  expiresAt: string | null;
   isLoadingAuth: boolean;
 }
 
@@ -27,7 +30,7 @@ const AuthContext = createContext<AuthContextType>({
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const supabase = createClient();
 
-  const { data: user, isLoading: isLoadingUser } = useQuery({
+  const { data: user, isLoading: isLoadingUser } = useQuery<User | null>({
     queryFn: async () => {
       const {
         data: { user },
@@ -53,16 +56,17 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     refetchOnReconnect: false,
   });
 
-  const { data: company, isLoading: isLoadingCompany } = useQuery({
-    queryFn: () => getCompanyById({ companyId: profile!.company_id }),
-    queryKey: ["company", profile?.company_id],
-    enabled: !!profile?.company_id,
-    staleTime: Infinity,
-    gcTime: Infinity,
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
-  });
+  const { data: company, isLoading: isLoadingCompany } =
+    useQuery<CompanyType | null>({
+      queryFn: () => getCompanyById({ companyId: profile!.company_id! }),
+      queryKey: ["company", profile?.company_id],
+      enabled: !!profile?.company_id,
+      staleTime: Infinity,
+      gcTime: Infinity,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    });
 
   const { data: subscription, isLoading: isLoadingSubscription } = useQuery({
     queryFn: () => {
@@ -100,7 +104,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     });
 
     return () => authSubscription.unsubscribe();
-  }, []);
+  }, [supabase]);
 
   useEffect(() => {
     if (!profile?.company_id) return;
@@ -113,11 +117,11 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
           event: "*",
           schema: "public",
           table: "company_subscriptions",
-          filter: `company_id=eq.${profile!.company_id}`,
+          filter: `company_id=eq.${profile.company_id}`,
         },
         () => {
           queryClient.invalidateQueries({
-            queryKey: ["company-subscription", profile!.company_id],
+            queryKey: ["company-subscription", profile.company_id],
           });
         }
       )
@@ -126,7 +130,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     return () => {
       supabase.removeChannel(channel);
     };
-  }, [profile?.company_id]);
+  }, [profile?.company_id, supabase]);
 
   const isLoadingAuth =
     isLoadingUser ||
@@ -135,13 +139,13 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     (!!company && isLoadingSubscription);
 
   const isPremium = subscription?.hasActiveSubscription ?? false;
-  const expiresAt = subscription?.expiresAt ?? false;
+  const expiresAt = subscription?.expiresAt ?? null;
 
   return (
     <AuthContext.Provider
       value={{
-        profile,
-        company,
+        profile: profile ?? null,
+        company: company ?? null,
         isPremium,
         expiresAt,
         isLoadingAuth,
