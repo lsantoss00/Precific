8194965bdf770aa03a94dcfc1e1a16a0ca2e094d
fsvboas <fsvboas.dev@gmail.com>
2026-01-17 02:05:49 -0300

    refactor: add shrink-0 class to coming-soon-badge component

--- src/components/coming-soon-badge.tsx
+++ src/components/coming-soon-badge.tsx
@@ -2,7 +2,7 @@ import Row from "@/src/components/core/row";
 
 const ComingSoonBadge = () => {
   return (
-    <Row className="rounded-full bg-neutral-900 p-2 h-2 w-fit items-center">
+    <Row className="rounded-full bg-neutral-900 p-2 h-2 w-fit items-center shrink-0">
       <span className="text-neutral-50 text-xs">Em breve</span>
     </Row>
   );
