5905f9d0fc19be01eed80fbd40378edbaab602df
fsvboas <fsvboas.dev@gmail.com>
2026-01-28 17:47:29 -0300

    feat: create a empty state to dashboard page

--- src/app/(private)/dashboard/components/dashboard-page-content.tsx
+++ src/app/(private)/dashboard/components/dashboard-page-content.tsx
@@ -18,18 +18,24 @@ import ComingSoonBadge from "@/src/components/coming-soon-badge";
 import Column from "@/src/components/core/column";
 import Flex from "@/src/components/core/flex";
 import Row from "@/src/components/core/row";
-import { LayoutDashboard, TriangleAlert } from "lucide-react";
+import Show from "@/src/components/core/show";
+import { useAuth } from "@/src/providers/auth-provider";
+import { CircleX, LayoutDashboard, TriangleAlert } from "lucide-react";
 import { useState } from "react";
 
 const DashboardPageContent = () => {
+  const { company } = useAuth();
+
   const [filters, setFilters] = useState<ChartFiltersType>({
     fromDate: undefined,
     toDate: undefined,
     productIds: undefined,
   });
 
+  const companyHasProducts = company ? company.productsQuantity > 0 : null;
+
   return (
-    <Column className="gap-4 relative">
+    <Column className="gap-4 relative flex-1">
       <Flex className="2xl:items-center gap-2 justify-between flex-col-reverse 2xl:flex-row">
         <Row className="items-center gap-2">
           <LayoutDashboard size={26} className="shrink-0" />
@@ -44,27 +50,51 @@ const DashboardPageContent = () => {
           </span>
         </Flex>
       </Flex>
-      <Column className="gap-4">
-        <DashboardFilters value={filters} onChange={setFilters} />
-        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 w-full gap-4">
-          <ProductsAveragePriceKpiCard filters={filters} />
-          <ProductsAverageAcquisitionCostKpiCard filters={filters} />
-          <ProductsAverageNetProfitKpiCard filters={filters} />
-          <ProductsAverageProfitabilityKpiCard filters={filters} />
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <ProductsMarkupRankingChart filters={filters} />
-          <ProductsNetProfitRankingChart filters={filters} />
-          <ProductsShippingRankingChart filters={filters} />
-          <ProductsFixedCostsRankingChart filters={filters} />
-          <ProductsPricesAndAcquisitionCostsChart
-            productIds={filters.productIds!}
-          />
-          <ProductsPricesAndNetProfitsChart productIds={filters.productIds!} />
-          <ProductsPriceHistoryChart filters={filters} />
-          <ProductsNetProfitHistoryChart filters={filters} />
-        </div>
-      </Column>
+      <Show
+        when={companyHasProducts}
+        fallback={
+          <Column className="grow items-center justify-center gap-4">
+            <div className="relative">
+              <LayoutDashboard size={64} />
+              <CircleX
+                size={32}
+                className="absolute -right-3 -bottom-2 bg-red-400 text-white rounded-full"
+              />
+            </div>
+            <Column className="text-center gap-2">
+              <p className="text-lg md:text-xl">Seu dashboard está vazio...</p>
+              <span className="text-sm md:text-base text-muted-foreground">
+                Assim que você adicionar um produto, as informações vão aparecer
+                aqui.
+              </span>
+            </Column>
+          </Column>
+        }
+      >
+        <Column className="gap-4">
+          <DashboardFilters value={filters} onChange={setFilters} />
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 w-full gap-4">
+            <ProductsAveragePriceKpiCard filters={filters} />
+            <ProductsAverageAcquisitionCostKpiCard filters={filters} />
+            <ProductsAverageNetProfitKpiCard filters={filters} />
+            <ProductsAverageProfitabilityKpiCard filters={filters} />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <ProductsMarkupRankingChart filters={filters} />
+            <ProductsNetProfitRankingChart filters={filters} />
+            <ProductsShippingRankingChart filters={filters} />
+            <ProductsFixedCostsRankingChart filters={filters} />
+            <ProductsPricesAndAcquisitionCostsChart
+              productIds={filters.productIds!}
+            />
+            <ProductsPricesAndNetProfitsChart
+              productIds={filters.productIds!}
+            />
+            <ProductsPriceHistoryChart filters={filters} />
+            <ProductsNetProfitHistoryChart filters={filters} />
+          </div>
+        </Column>
+      </Show>
     </Column>
   );
 };
