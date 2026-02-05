10caf2c1562380a8219416ed0461fa38dbd3cda7
fsvboas <fsvboas.dev@gmail.com>
2026-02-05 13:04:25 -0300

    fix: hyratation error in custom tooltip + sortable header

--- src/components/custom-tooltip.tsx
+++ src/components/custom-tooltip.tsx
@@ -1,6 +1,5 @@
 import { CircleQuestionMark } from "lucide-react";
 import { useState } from "react";
-import { Button } from "./core";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./core/tooltip";
 
 interface CustomTooltipProps {
@@ -21,8 +20,7 @@ const CustomTooltip = ({
   return (
     <Tooltip open={openTooltip} onOpenChange={setOpenTooltip}>
       <TooltipTrigger asChild>
-        <Button
-          type="button"
+        <span
           tabIndex={-1}
           className={`${className} bg-transparent shadow-none text-foreground hover:bg-transparent h-fit w-fit! cursor-default p-0! m-0!`}
           onClick={() => setOpenTooltip(!openTooltip)}
@@ -35,7 +33,7 @@ const CustomTooltip = ({
           }}
         >
           {icon}
-        </Button>
+        </span>
       </TooltipTrigger>
       <TooltipContent
         align="end"
