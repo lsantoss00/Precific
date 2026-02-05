04ff3144db49c0c2bee4b8abaff65fc74eb17bff
fsvboas <fsvboas.dev@gmail.com>
2026-02-04 22:10:11 -0300

    refactor: new style to auth guard pending

--- src/components/auth-guard.tsx
+++ src/components/auth-guard.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import shortLogoImage from "@/public/images/precific-short-logo-image.webp";
 import { useAuth } from "@/src/providers/auth-provider";
 import { Loader2 } from "lucide-react";
+import Image from "next/image";
 import { ReactNode } from "react";
 
 interface AuthGuardProps {
@@ -12,7 +14,20 @@ export function AuthGuard({ children }: AuthGuardProps) {
   const { isLoadingAuth } = useAuth();
 
   if (isLoadingAuth)
-    return <Loader2 className="text-primary animate-spin m-auto w-10 h-10" />;
+    return (
+      <div className="m-auto relative">
+        <Loader2 className="text-primary animate-spin w-12 h-12" />
+        <Image
+          src={shortLogoImage}
+          alt="Precific | Plataforma de Precificação Inteligente"
+          width={24}
+          height={24}
+          sizes="24px"
+          priority
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-pulse"
+        />
+      </div>
+    );
 
   return <>{children}</>;
 }
