bc622a8d7170fdff2407557139df9c62a6e6daba
fsvboas <fsvboas.dev@gmail.com>
2026-02-12 09:48:55 -0300

    refactor: re-add animations from hero-section component

--- src/app/(landing)/components/hero-section.tsx
+++ src/app/(landing)/components/hero-section.tsx
@@ -1,3 +1,4 @@
+import FadeInOnScroll from "@/src/components/animations/fade-in-on-scroll";
 import { Container } from "@/src/components/core";
 import Column from "@/src/components/core/column";
 import Flex from "@/src/components/core/flex";
@@ -14,41 +15,53 @@ const HeroSection = () => {
     >
       <Container as="section" variant="section" className="xl:h-full">
         <Flex className="flex-col xl:flex-row items-center justify-between xl:h-full gap-8 md:gap-12">
-          <Column className="space-y-6 md:space-y-8">
-            <Column as="header" className="space-y-4 md:space-y-5">
-              <h1 className="sr-only">
-                Precific | Plataforma de Precificação Inteligente
-              </h1>
-              <h2 className="font-bold text-3xl sm:text-4xl md:text-5xl xl:text-5xl 2xl:text-6xl text-white leading-tight">
-                <span className="relative inline-block">
-                  Precifique
-                  <span
-                    className="absolute bottom-1 left-4 h-4 w-full bg-linear-to-r from-secondary to-transparent -z-10"
-                    aria-hidden="true"
-                  />
-                </span>{" "}
-                seus produtos com precisão.
-              </h2>
-              <p className="text-zinc-200 text-base md:text-lg max-w-2xl">
-                O <strong className="font-normal">Precific</strong> automatiza o
-                cálculo de preços com base em custos, impostos e margens,
-                simulando cenários futuros da Reforma Tributária.
-              </p>
-            </Column>
-            {/* <Button
+          <FadeInOnScroll
+            direction="left"
+            offset={20}
+            className="relative z-10 w-full xl:max-w-140"
+          >
+            <Column className="space-y-6 md:space-y-8">
+              <Column as="header" className="space-y-4 md:space-y-5">
+                <h1 className="sr-only">
+                  Precific | Plataforma de Precificação Inteligente
+                </h1>
+                <h2 className="font-bold text-3xl sm:text-4xl md:text-5xl xl:text-5xl 2xl:text-6xl text-white leading-tight">
+                  <span className="relative inline-block">
+                    Precifique
+                    <span
+                      className="absolute bottom-1 left-4 h-4 w-full bg-linear-to-r from-secondary to-transparent -z-10"
+                      aria-hidden="true"
+                    />
+                  </span>{" "}
+                  seus produtos com precisão.
+                </h2>
+                <p className="text-zinc-200 text-base md:text-lg max-w-2xl">
+                  O <strong className="font-normal">Precific</strong> automatiza
+                  o cálculo de preços com base em custos, impostos e margens,
+                  simulando cenários futuros da Reforma Tributária.
+                </p>
+              </Column>
+              {/* <Button
               className="w-full sm:w-fit h-12 md:h-14 px-6 md:px-8 hover:cursor-pointer font-medium text-sm md:text-base"
               variant="secondary"
               onClick={() => scrollToSection("contato")}
             >
               Agendar Demonstração
             </Button> */}
-          </Column>
-          <Flex as="figure" className="aspect-video shrink-0">
-            <YouTubeEmbed
-              videoId="9gNKBYR-rhg"
-              title="Precific — Demonstração do sistema de precificação inteligente"
-            />
-          </Flex>
+            </Column>
+          </FadeInOnScroll>
+          <FadeInOnScroll
+            direction="right"
+            offset={20}
+            className="relative z-10 w-full xl:max-w-180 2xl:max-w-240 mt-8 xl:mt-0"
+          >
+            <Flex as="figure" className="aspect-video shrink-0">
+              <YouTubeEmbed
+                videoId="9gNKBYR-rhg"
+                title="Precific — Demonstração do sistema de precificação inteligente"
+              />
+            </Flex>
+          </FadeInOnScroll>
         </Flex>
       </Container>
     </SectionWithBackground>
