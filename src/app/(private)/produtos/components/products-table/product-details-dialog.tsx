97d2e74470255a87bb3e81800106e99ff02749e9
fsvboas <fsvboas.dev@gmail.com>
2026-02-11 23:11:42 -0300

    feat: add responsiveness to product-details-dialog component

--- src/app/(private)/produtos/components/products-table/product-details-dialog.tsx
+++ src/app/(private)/produtos/components/products-table/product-details-dialog.tsx
@@ -12,10 +12,12 @@ import {
   Separator,
 } from "@/src/components/core";
 import Column from "@/src/components/core/column";
-import Row from "@/src/components/core/row";
+import Flex from "@/src/components/core/flex";
 import Show from "@/src/components/core/show";
 import CustomTooltip from "@/src/components/custom-tooltip";
+import StatusBadge from "@/src/components/status-badge";
 import { dateFormatter } from "@/src/helpers/date-formatter";
+import { useMediaQuery } from "@/src/hooks/use-media-query";
 import { useAuth } from "@/src/providers/auth-provider";
 import { DialogTitle } from "@radix-ui/react-dialog";
 import { CircleAlert } from "lucide-react";
@@ -32,6 +34,7 @@ const ProductDetailsDialog = ({
   product,
 }: ProductDetailsDialogProps) => {
   const { company } = useAuth();
+  const isMd = useMediaQuery(`(min-width: 768px)`);
 
   const isSimpleNational = company?.taxRegime === "simple_national";
 
@@ -95,7 +98,7 @@ const ProductDetailsDialog = ({
       title: "Preço de venda final",
       value: 10,
       variant: "secondary" as const,
-      gridSpan: "col-span-1 md:col-span-2",
+      gridSpan: "col-span-1 sm:col-span-2",
     },
   ];
 
@@ -106,6 +109,7 @@ const ProductDetailsDialog = ({
     {
       title: "Base de cálculo IBS/CBS",
       value: 10,
+      gridSpan: "col-span-1 sm:col-span-2 md:col-span-1",
     },
     {
       title: "IBS (0.1%)",
@@ -119,35 +123,39 @@ const ProductDetailsDialog = ({
       title: "Preço de venda final",
       value: 10,
       variant: "secondary" as const,
-      gridSpan: "col-span-1 md:col-span-3",
+      gridSpan: "col-span-1 sm:col-span-2 md:col-span-3",
     },
   ];
 
+  const ncm = product?.ncm
+    ?.toString()
+    .replace(/^(\d{4})(\d{2})(\d{2})$/, "$1.$2.$3");
+
   return (
     <Dialog open={open} onOpenChange={onOpenChange} modal>
-      <DialogContent className="flex flex-col md:min-w-4xl md:p-8 overflow-y-auto h-[96dvh]">
-        <DialogHeader className="gap-4">
-          <Column className="gap-1">
-            <DialogTitle className="text-2xl">{product?.name}</DialogTitle>
-            <Row className="gap-2">
-              <span className="text-sm text-muted-foreground">
+      <DialogContent className="flex flex-col min-w-[96dvw] lg:min-w-4xl py-10 sm:p-8 overflow-y-auto h-[96dvh]">
+        <DialogHeader className="sm:gap-4 text-start">
+          <Column className="sm:gap-1">
+            <DialogTitle className="text-2xl text-ellipsis truncate">
+              {product?.name}
+            </DialogTitle>
+            <Flex className="flex-col sm:flex-row gap-2">
+              <span className="text-sm text-muted-foreground  text-ellipsis truncate">
                 {product?.sku}
               </span>
               <Show when={Boolean(product?.ncm)}>
-                <Separator orientation="vertical" />
-                {/* TO-DO: NCM FORMATTER */}
-                <span className="text-sm text-muted-foreground">
-                  {product?.ncm}
-                </span>
+                <Show when={isMd}>
+                  <Separator orientation="vertical" />
+                </Show>
+                <span className="text-sm text-muted-foreground">{ncm}</span>
               </Show>
-              <Separator orientation="vertical" />
-              {/* TO-DO: BADGE CUSTOMIZADA ATIVADO/DESATIVADO */}
-              <span className="text-sm text-muted-foreground">
-                {product?.status}
-              </span>
-            </Row>
+              <Show when={isMd}>
+                <Separator orientation="vertical" />
+              </Show>
+              <StatusBadge status={product?.status!} />
+            </Flex>
           </Column>
-          <Row className="gap-8">
+          <Flex className="flex-col sm:flex-row gap-2 sm:gap-8 text-start">
             <Column>
               <span className="text-sm text-muted-foreground">
                 Data de Criação:
@@ -160,7 +168,7 @@ const ProductDetailsDialog = ({
               </span>
               <span>{dateFormatter(product?.updatedAt!, true)}</span>
             </Column>
-          </Row>
+          </Flex>
           <Column>
             <span className="text-sm text-muted-foreground">Observações:</span>
             <p>
@@ -173,7 +181,7 @@ const ProductDetailsDialog = ({
           <span>
             Pré-Reforma Tributária <strong>2025</strong>
           </span>
-          <div className="grid grid-cols-4 gap-2">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-2">
             {metrics2025
               .filter(
                 (metric) => metric.condition === undefined || metric.condition,
@@ -193,16 +201,17 @@ const ProductDetailsDialog = ({
         </Column>
         <Separator />
         <Column className="space-y-4 flex-1">
-          <Row className="gap-1 items-center">
-            <h3 className="text-lg">
-              Transição Reforma Tributária <strong>2026</strong>
-            </h3>
-            <CustomTooltip
-              icon={<CircleAlert className="w-4 h-4" />}
-              message="O valor de IBS/CBS é exibido para transparência fiscal, conforme Art. 348, § 1º. O recolhimento deste tributo não é de responsabilidade do contribuinte nesta nota, sendo o destaque meramente informativo."
-            />
-          </Row>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <h3 className="text-lg">
+            Transição Reforma Tributária{" "}
+            <span className="inline-flex items-center gap-2">
+              <strong>2026</strong>
+              <CustomTooltip
+                icon={<CircleAlert className="w-4 h-4" />}
+                message="O valor de IBS/CBS é exibido para transparência fiscal, conforme Art. 348, § 1º. O recolhimento deste tributo não é de responsabilidade do contribuinte nesta nota, sendo o destaque meramente informativo."
+              />
+            </span>
+          </h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {metrics2026.map((metric, index) => (
               <div key={`metric-2026-${index}`} className={metric.gridSpan}>
                 <MetricCard
